c9b105b41333a66425ec6346361f617aab63372e
NickPugs <63168497+NickPugs@users.noreply.github.com>
2020-08-24 14:53:57 -0500

    updates

---
+++ js/membership_opportunities.js
@@ -0,0 +1,11 @@
+$("#toggleName").on("click", function(){
+    $("#dropdown").slideToggle();
+});
+
+$("#toggle2name").on("click", function(){
+    $("#toggle2").slideToggle();
+});
+
+$("#toggle3name").on("click", function(){
+    $("#toggle3").slideToggle();
+});
