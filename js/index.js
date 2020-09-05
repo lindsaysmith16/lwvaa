067e64ab372aefe7a9e70e36df27048208176b45
David W. Ciccocioppo <63469174+dwciccodesign@users.noreply.github.com>
2020-09-05 12:26:16 -0500

    Added dropdown to main index deleted test files

--- js/index.js
+++ js/index.js
@@ -2,3 +2,12 @@ $("#education").click(function(){
     window.location = $(this).find("a").attr("href");
     return false;
 })
+
+$(document).ready(function(){
+    $("#menu, #menu2, #menu3, #menu4").hover(function(){
+      $(".dropdown-content", this).slideDown(250);
+    }, function(){
+      $(".dropdown-content", this).stop().slideUp(100);
+    });
+  })
+  
